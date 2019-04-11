fe70e95132f17b9113cfd57a5a83963496718f16
yzhukovs <yzhukovs@bumnetworks.com>
2019-04-11 16:21:00 -0400

    initial commit

--- projects/graph/social/social.py
+++ projects/graph/social/social.py
@@ -1,133 +1,137 @@
 import random
-import queue
+import math
+import time
+
+class Queue:
+	def __init__(self):
+		self.queue = []
+	def enqueue(self, value):
+		self.queue.append(value)
+	def dequeue(self):
+		if (self.size) > 0:
+			return self.queue.pop(0)
+		else:
+			return None
+	def size(self):
+		return len(self.queue)
 
 class User:
-    def __init__(self, name):
-        self.name = name
+	def __init__(self, name):
+		self.name = name
 
 class SocialGraph:
-    def __init__(self):
-        self.lastID = 0
-        self.users = {}
-        self.friendships = {}
+	def __init__(self):
+		self.lastID = 0
+		self.users = {}
+		self.friendships = {}
 
-    def addFriendship(self, userID, friendID):
-        """
-        Creates a bi-directional friendship
-        """
-        if userID == friendID:
-            print("WARNING: You cannot be friends with yourself")
-        elif friendID in self.friendships[userID] or userID in self.friendships[friendID]:
-            print("WARNING: Friendship already exists")
-        else:
-            self.friendships[userID].add(friendID)
-            self.friendships[friendID].add(userID)
+	def addFriendship(self, userID, friendID):
+		if userID == friendID:
+			pass
+			# print("You cannot be friends with yourself")
+		elif friendID in self.friendships[userID] or userID in self.friendships[friendID]:
+			pass
+			# print("You already are friends with this user")
+		else:
+			self.friendships[userID].add(friendID)
+			self.friendships[friendID].add(userID)
+			return True
+	
+	def addUser(self, name):
+		self.lastID += 1
+		self.users[self.lastID] = User(name)
+		self.friendships[self.lastID] = set()
 
-    def addUser(self, name):
-        """
-        Create a new user with a sequential integer ID
-        """
-        self.lastID += 1  # automatically increment the ID to assign the new user
-        self.users[self.lastID] = User(name)
-        self.friendships[self.lastID] = set()
+	def populateGraph(self, numUsers, avgFriendships):
+		self.lastID = 0
+		self.users = {}
+		self.friendships = {}
+		for i in range(0, numUsers):
+			self.addUser(f"User {i}")
+		possibleFriendships = []
+		for userID in self.users:
+			for friendID in range(userID + 1, self.lastID + 1):
+				possibleFriendships.append((userID, friendID))
+		random.shuffle(possibleFriendships)
+		for i in range(0, (numUsers * avgFriendships // 2)):
+			friendship = possibleFriendships[i]
+			self.addFriendship(friendship[0], friendship[1])
 
-    def populateGraph(self, numUsers, avgFriendships):
-        """
-        Takes a number of users and an average number of friendships
-        as arguments
+	def populateGraphLinear(self, numUsers, avgFriendships):
+		self.lastID = 0
+		self.users = {}
+		self.friendships = {}
 
-        Creates that number of users and a randomly distributed friendships
-        between those users.
+		for i in range(numUsers):
+			self.addUser(f"User {i + 1}")
 
-        The number of users must be greater than the average number of friendships.
-        """
-        # Reset graph
-        self.lastID = 0
-        self.users = {}
-        self.friendships = {}
-        # !!!! IMPLEMENT ME
+		targetFriendships = (numUsers * avgFriendships) // 2
+		totalFriendships = 0
+		collisions = 0
+		while totalFriendships < targetFriendships:
+			userID = random.randint(1, self.lastID)
+			friendID = random.randint(1, self.lastID)
+			if self.addFriendship(userID, friendID):
+				totalFriendships += 2
+			else:
+				collisions += 1
+		print(f"COLLISIONS: {collisions}")
 
-        for i in range(0, numUsers):
-            self.addUser(f'User{i}')
-        # Create friendships
-        possibleFriendships = []
-        for userID in self.users:
-            for friendID in range(userID + 1, self.lastID + 1):
-                possibleFriendships.append((userID, friendID))
-        # Shuffle the possible friendships
-        random.shuffle(possibleFriendships)
+	def getAllSocialPaths(self, userID):
+		visited = {}
+		q = Queue()
+		q.enqueue([userID])
+		while q.size() > 0:
+			path = q.dequeue()
+			newUserID = path[-1]
+			if newUserID not in visited:
+				visited[newUserID] = path
+				for friendID in self.friendships[newUserID]:
+					if friendID not in visited:
+						new_path = list(path)
+						new_path.append(friendID)
+						q.enqueue(new_path)
+		return visited
 
-       
-        for i in range(numUsers * avgFriendships // 2):
-            friendship = possibleFriendships[i]
-            self.addFriendship(friendship[0], friendship[1])        
+# if __name__ == '__main__':
+# 	sg = SocialGraph()
+# 	start_time = time.time()
+# 	sg.populateGraph(1000, 5)
+# 	end_time = time.time()
+# 	print (f"runtime: {end_time = start_time} seconds")
+# 	connections = sg.getAllSocialPaths(1)
+# 	total = 0
+# 	for userID in connections:
+# 		total += len(connections[userID]) - 1
+# 		print(len(connections))
+# 		print(total / len(connections))
 
-        # Create friendships
-
-    def getAllSocialPaths(self, userID):
-        """
-        Takes a user's userID as an argument
-
-        Returns a dictionary containing every user in that user's
-        extended network with the shortest friendship path between them.
-
-        The key is the friend's ID and the value is the path.
-        """
-        visited = {}  # Note that this is a dictionary, not a set
-        # !!!! IMPLEMENT ME
-        if userID not in self.friendships:
-            return None
-
-        q = Queue()
-        q.enqueue([userID])
-
-        while q.size() > 0:
-            path = q.dequeue()
-            vertex = path[-1] # last node in path
-            if vertex not in visited:
-                visited[vertex] = path
-
-                for next_vertex in self.friendships[vertex]:
-                    new_path = path.copy()
-                    new_path.append(next_vertex)
-                    q.enqueue(new_path)
-
-
-        return visited
-
-    # def bfs_path(self, starting_vertex_id, target_value):
-    #     q = Queue() 
-    #     q.enqueue([starting_vertex_id])
-    #     visited = set()
-    #     while q.size()> 0:
-    #         path = q.dequeue()
-    #         v = path[-1] #last vertex
-    #         if v not in visited:
-    #             if v == target_value:
-    #                 return path
-    #             visited.append(v)
-    #             for next_vert in self.vertices[v]
-    #                 new_path = list(path)
-    #                 new_path.append(next_vert)
-    #                 q.enqueue(new_path)
-    #     return None
-
-class Queue():
-    def __init__(self):
-        self.queue = []
-    def enqueue(self, value):
-        self.queue.append(value)
-    def dequeue(self):
-        if self.size() > 0:
-            return self.queue.pop(0)
-        else:
-            return None
-    def size(self):
-        return len(self.queue)    
+# 	totalConnections = 0
+# 	totalDegrees = 0
+# 	iterations = 10
+# 	for i in range(0, iterations):
+# 		sg.populateGraph(1000, 5)
+# 		connections = sg.getAllSocialPaths(1)
+# 		total = 0
+# 		for userID in connections:
+# 			total += len(connections[userID]) - 1
+# 		totalConnections += len(connections)
+# 		totalDegrees += total / len(connections)
+# 		print("-------")
+# 		print(f"Friends in network: {len(connections)}")
+# 		print(f"Avg degrees: {total / len(connections)}")
+# 	print(totalConnections / iterations)
+# 	print(totalDegrees / iterations)
 
 if __name__ == '__main__':
-    sg = SocialGraph()
-    sg.populateGraph(10, 2)
-    print(sg.friendships)
-    connections = sg.getAllSocialPaths(1)
-    print(connections)
+	sg = SocialGraph()
+	numUsers = 10000
+	avgFriendships = 9999
+	linear_start_time = time.time()
+	sg.populateGraphLinear(numUsers, avgFriendships)
+	linear_end_time = time.timme()
+	print(f"Linear runtime: {linear_end_time - linear_start_time} seconds")
+	q_start_time = time.time()
+	sg.populateGraph(numUsers, avgFriendships)
+	q_end_time = time.time()
+	print(f"Quadratic runtime: {q_end_time - q_start_time} seconds")
